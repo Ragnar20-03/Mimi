24f295f0ba65e875980c0d91c8cff33af9ccfed5
Ragnar20-03 <roshanpp20@gmail.com>
2024-12-26 16:02:18 +0530

    Added Index.js :

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,20 @@
 const express = require("express");
 const { PORT } = require("./Config/dotenv");
+const cors = require("cors");
 
 const userRouter = require("./Routes/user");
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 
 app.use("/user", userRouter);
+
+app.get("/me", (req, res) => {
+  res.status(200).json({
+    msg: "Jay Ganesh !",
+  });
+});
 
 app.listen(PORT, async () => {
   console.log("Server Stared on Port number : ", PORT);
