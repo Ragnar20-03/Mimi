29e34b9e52b1677565ddb5dd9eeabcec71b0f3df
Ragnar20-03 <roshanpp20@gmail.com>
2024-12-26 13:32:48 +0530

    Project Structure

---
+++ backend/Routes/user.js
@@ -0,0 +1,28 @@
+const express = require("express");
+
+// Auth :
+// register -> getOtp -> verifyotp ( usrename , fname ,lname ,password , )
+
+// login -> username  password
+
+// /-----------------
+//////////////////////////////////////////////////////
+////////////////////////////////////
+// Home
+//  get allposts -> getPosts .
+//  get sepcifucPost -> getpostById
+
+// like post ,
+// commentpost
+
+// view Profile
+//////////////////////////////////////////////////////
+
+// follow
+// unfollow
+
+////////////////////////////////////
+
+// Profile //////////
+// Get Profile
+// updateprofile
